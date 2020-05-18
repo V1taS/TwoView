cbdc5aa8b9d5564c262994950b6413333ecee874
Vitalii Sosin <375693@mail.ru>
2020-05-18 23:41:10 +0300

    Initial Commit

---
+++ TwoView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwoView
+//
+//  Created by Виталий Сосин on 18.05.2020.
+//  Copyright © 2020 Vitalii Sosin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
